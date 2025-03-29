d17799b8d13c5cad6a1fcf6f0e32ad64f9581bbb
Mirza Sharmin <mirza.sharmin1@gmail.com>
2025-03-29 19:01:46 +0000

    Added modules

--- webapps2025/settings.py
+++ webapps2025/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'register',
+    'payapp'
 ]
 
 MIDDLEWARE = [
@@ -77,7 +79,7 @@ WSGI_APPLICATION = 'webapps2025.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'webapps.db',
     }
 }
 
